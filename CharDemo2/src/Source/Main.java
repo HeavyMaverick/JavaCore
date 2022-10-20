94c86d6c3e7f184ddd76cd9701eb621ae1982768
HeavyMaverick <snoopyleet10@gmail.com>
2022-10-20 20:20:51 +0700

    CharDemo2
    Another test

---
+++ CharDemo2/src/Source/Main.java
@@ -0,0 +1,11 @@
+package Source;
+
+public class Main {
+    public static void main(String[] args){
+        char ch1;
+        ch1 = 'X';
+        System.out.println("ch1 Содержит " + ch1);
+        ch1++;
+        System.out.println("Теперь ch1 содержит " + ch1);
+    }
+}
